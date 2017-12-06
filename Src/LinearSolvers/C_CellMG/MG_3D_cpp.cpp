6f4e25a42e1fbf786917b40fdc322c816dfc60dd
Brian Friesen <bfriesen@lbl.gov>
2017-12-06 13:50:11 -0800

    C_CellMG: RAJA-ify C_GSRB_3D() (sort of)
    Since I can't figure out how to generate index sets for red-black iterations in
    a sane way, right now this kernel just does pure Gauss-Seidel (i.e., the i loop
    does stride-1, not stride-2).

--- Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
+++ Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
@@ -142,43 +142,51 @@ const Real* h)
 	Real dhx = beta/(h[0]*h[0]);
 	Real dhy = beta/(h[1]*h[1]);
 	Real dhz = beta/(h[2]*h[2]);
-	
-	for (int n = 0; n<nc; n++){
-		for (int k = lo[2]; k <= hi[2]; ++k) {
-			for (int j = lo[1]; j <= hi[1]; ++j) {
-				int ioff = (lo[0] + j + k + rb)%2;
-#pragma omp simd
-				for (int i = lo[0] + ioff; i <= hi[0]; i+=2) {
-					
-					//BC terms
-					Real cf0 = ( (i==blo[0]) && (m0(IntVect(blo[0]-1,j,k))>0) ? f0(IntVect(blo[0],j,k)) : 0. );
-					Real cf1 = ( (j==blo[1]) && (m1(IntVect(i,blo[1]-1,k))>0) ? f1(IntVect(i,blo[1],k)) : 0. );
-					Real cf2 = ( (k==blo[2]) && (m2(IntVect(i,j,blo[2]-1))>0) ? f2(IntVect(i,j,blo[2])) : 0. );
-					Real cf3 = ( (i==bhi[0]) && (m3(IntVect(bhi[0]+1,j,k))>0) ? f3(IntVect(bhi[0],j,k)) : 0. );
-					Real cf4 = ( (j==bhi[1]) && (m4(IntVect(i,bhi[1]+1,k))>0) ? f4(IntVect(i,bhi[1],k)) : 0. );
-					Real cf5 = ( (k==bhi[2]) && (m5(IntVect(i,j,bhi[2]+1))>0) ? f5(IntVect(i,j,bhi[2])) : 0. );
-					
-					//assign ORA constants
-					double gamma = alpha * a(IntVect(i,j,k))
-									+ dhx * (bX(IntVect(i,j,k)) + bX(IntVect(i+1,j,k)))
-									+ dhy * (bY(IntVect(i,j,k)) + bY(IntVect(i,j+1,k)))
-									+ dhz * (bZ(IntVect(i,j,k)) + bZ(IntVect(i,j,k+1)));
-					
-					double g_m_d = gamma
-									- dhx * (bX(IntVect(i,j,k))*cf0 + bX(IntVect(i+1,j,k))*cf3)
-									- dhy * (bY(IntVect(i,j,k))*cf1 + bY(IntVect(i,j+1,k))*cf4)
-									- dhz * (bZ(IntVect(i,j,k))*cf2 + bZ(IntVect(i,j,k+1))*cf5);
-					
-					double rho =  dhx * (bX(IntVect(i,j,k))*phi(IntVect(i-1,j,k),n) + bX(IntVect(i+1,j,k))*phi(IntVect(i+1,j,k),n))
-								+ dhy * (bY(IntVect(i,j,k))*phi(IntVect(i,j-1,k),n) + bY(IntVect(i,j+1,k))*phi(IntVect(i,j+1,k),n))
-								+ dhz * (bZ(IntVect(i,j,k))*phi(IntVect(i,j,k-1),n) + bZ(IntVect(i,j,k+1))*phi(IntVect(i,j,k+1),n));
-					
-					double res = rhs(IntVect(i,j,k),n) - gamma * phi(IntVect(i,j,k),n) + rho;
-					phi(IntVect(i,j,k),n) += omega/g_m_d * res;
-				}
-			}
-		}
-	}
+
+        RAJA::RangeSegment iBounds(lo[0], hi[0]+1);
+        RAJA::RangeSegment jBounds(lo[1], hi[1]+1);
+        RAJA::RangeSegment kBounds(lo[2], hi[2]+1);
+        RAJA::RangeSegment nBounds(0, nc);
+
+	// This is technically just Gauss-Seidel, not the red-black variation.
+	// The index set for i depends on the values of j and k; I don't know
+	// how to capture that without pre-computing unique index sets for i,
+	// given all combinations of j and k. There must be a simpler way to do
+	// it than that, because ultimately the i loop is starting either at
+	// lo[0] or lo[0]+1.
+
+        RAJA::forallN<RAJA::NestedPolicy<
+          RAJA::ExecList<RAJA::seq_exec, RAJA::seq_exec, RAJA::seq_exec, RAJA::seq_exec>>> (
+            iBounds, jBounds, kBounds, nBounds, [&](int i, int j, int k, int n) {
+
+          //BC terms
+          Real cf0 = ( (i==blo[0]) && (m0(IntVect(blo[0]-1,j,k))>0) ? f0(IntVect(blo[0],j,k)) : 0. );
+          Real cf1 = ( (j==blo[1]) && (m1(IntVect(i,blo[1]-1,k))>0) ? f1(IntVect(i,blo[1],k)) : 0. );
+          Real cf2 = ( (k==blo[2]) && (m2(IntVect(i,j,blo[2]-1))>0) ? f2(IntVect(i,j,blo[2])) : 0. );
+          Real cf3 = ( (i==bhi[0]) && (m3(IntVect(bhi[0]+1,j,k))>0) ? f3(IntVect(bhi[0],j,k)) : 0. );
+          Real cf4 = ( (j==bhi[1]) && (m4(IntVect(i,bhi[1]+1,k))>0) ? f4(IntVect(i,bhi[1],k)) : 0. );
+          Real cf5 = ( (k==bhi[2]) && (m5(IntVect(i,j,bhi[2]+1))>0) ? f5(IntVect(i,j,bhi[2])) : 0. );
+
+          //assign ORA constants
+          double gamma = alpha * a(IntVect(i,j,k))
+                       + dhx * (bX(IntVect(i,j,k)) + bX(IntVect(i+1,j,k)))
+                       + dhy * (bY(IntVect(i,j,k)) + bY(IntVect(i,j+1,k)))
+                       + dhz * (bZ(IntVect(i,j,k)) + bZ(IntVect(i,j,k+1)));
+
+          double g_m_d = gamma
+                       - dhx * (bX(IntVect(i,j,k))*cf0 + bX(IntVect(i+1,j,k))*cf3)
+                       - dhy * (bY(IntVect(i,j,k))*cf1 + bY(IntVect(i,j+1,k))*cf4)
+                       - dhz * (bZ(IntVect(i,j,k))*cf2 + bZ(IntVect(i,j,k+1))*cf5);
+
+          double rho =  dhx * (bX(IntVect(i,j,k))*phi(IntVect(i-1,j,k),n) + bX(IntVect(i+1,j,k))*phi(IntVect(i+1,j,k),n))
+                     + dhy * (bY(IntVect(i,j,k))*phi(IntVect(i,j-1,k),n) + bY(IntVect(i,j+1,k))*phi(IntVect(i,j+1,k),n))
+                     + dhz * (bZ(IntVect(i,j,k))*phi(IntVect(i,j,k-1),n) + bZ(IntVect(i,j,k+1))*phi(IntVect(i,j,k+1),n));
+
+          double res = rhs(IntVect(i,j,k),n) - gamma * phi(IntVect(i,j,k),n) + rho;
+
+          phi(IntVect(i,j,k),n) += omega/g_m_d * res;
+
+        });
 }
 
 //-----------------------------------------------------------------------
